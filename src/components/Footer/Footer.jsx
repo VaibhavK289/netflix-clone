780bb5ce964f9af3f3643892c46e41b8a5c4c107
VaibhavK <vaibhavkrkandhway@gmail.com>
2025-02-18 00:37:58 +0530

    Day-1 end

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import './Footer.css'
+import Youtube_icons from '../../assets/youtube_icon.png'
+import twitter_icons from '../../assets/twitter_icon.png'
+import instagram_icons from '../../assets/instagram_icon.png'
+import facebook_icons from '../../assets/facebook_icon.png'
 
 const Footer = () => {
   return (
     <div className='footer'>
-        Footer
+        <div className="footer-icons">
+          <img src=[facebook_icon] alt="" />
+          <img src=[instagram_icon] alt="" />
+          <img src=[twitter_icon] alt="" />
+          <img src=[youtube_icon] alt="" />
+        </div>
     </div>
   )
 }

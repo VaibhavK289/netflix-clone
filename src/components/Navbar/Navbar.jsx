89b6e4fd11467d3be985a9de1fb019cf3ffe1030
VaibhavK <vaibhavkrkandhway@gmail.com>
2025-02-17 22:18:13 +0530

    navigation bar complete

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -27,6 +27,9 @@ const Navbar = () => {
         <div className="navbar-profile">
             <img src={profile_img} alt="" className="profile" />
             <img src={caret_icon} alt="" className="profile" />
+            <div className="dropdown">
+                <p>Sign Out Of Netflix</p>
+            </div>
         </div>
       </div>
     </div>
